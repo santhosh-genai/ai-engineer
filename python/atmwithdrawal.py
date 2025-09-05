3999ed955b94b51ed4a0a871cc60327dd4fa7ec0
Santhosh Manickam <mailtomsanthosh@gmail.com>
2025-09-05 23:45:59 +0530

    deleted

--- python/atmwithdrawal.py
+++
@@ -1,5 +0,0 @@
-amount = int(input("Enter the amount to withdraw: "))
-if amount % 100 == 0:
-    print(f"Dispensing {amount}")
-else:
-    print("Invalid amount")
--- python/employeename.py
+++
@@ -1,3 +0,0 @@
-employees = ["Alice", "Bob", "Charlie", "David", "Eve"]
-for index, name in enumerate(employees):
-    print(index+1, name)
