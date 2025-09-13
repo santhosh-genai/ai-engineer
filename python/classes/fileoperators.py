85d161fb533b10d6fc0ed3fc5a6767c1510ba183
Santhosh Manickam <mailtomsanthosh@gmail.com>
2025-09-13 18:02:42 +0530

    file

--- python/classes/fileoperators.py
+++ python/classes/fileoperators.py
@@ -1,13 +1,13 @@
-with open("C:\\Users\\2276038\\Desktop\\Learning\\classes\\report.txt", "w") as file:
+with open("C:\\Users\\2276038\\Desktop\\Learning\\python\\classes\\report.txt", "w") as file:
         file.write("TestCase1 - Passed")
         file.write("\nTestCase2 - Failed")
         file.write("\nTestCase3 - Passed")
 
-with open("C:\\Users\\2276038\\Desktop\\Learning\\classes\\report.txt", "a") as file:
+with open("C:\\Users\\2276038\\Desktop\\Learning\\python\\classes\\report.txt", "a") as file:
         file.write("\nTestCase4 - Failed")
         file.write("\nTestCase5 - Passed")
 
-with open("C:\\Users\\2276038\\Desktop\\Learning\\classes\\report.txt", "r") as file:
+with open("C:\\Users\\2276038\\Desktop\\Learning\\python\\classes\\report.txt", "r") as file:
         content = file.read()
         print(content)
         passed_count = 0
