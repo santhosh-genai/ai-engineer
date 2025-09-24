735d7f5a6ad0d3f41306d5e7bad75abc34ecae4e
Santhosh Manickam <mailtomsanthosh@gmail.com>
2025-09-24 23:47:43 +0530

    numpy

--- python/assignments/Assignment_week3/TestReport.py
+++ python/assignments/Assignment_week3/TestReport.py
@@ -1,17 +1,27 @@
 import numpy as np
 
 class TestReport:
-    execution_times = np.array([])
-
-class TestReport:
-
-    def average_time():
-        mean = np.mean(TestReport.execution_times)
+    def __init__(self, execution_times):
+        self.execution_times = execution_times
+        
+    def average_time(self):
+        mean = np.mean(self.execution_times)
         return mean
 
-    def max_time():
-        maximum = np.max(TestReport.execution_times)
+    def max_time(self):
+        maximum = np.max(self.execution_times)
         return maximum
     
-    def slow_test(threshold)
-    
+class RegressionReport(TestReport):
+    def __init__(self, execution_times):
+        super().__init__(execution_times)
+        
+    def slow_test(self, threshold):
+        slow_tests = self.execution_times[self.execution_times > threshold]
+        return slow_tests
+     
+tests = np.array([10, 15, 20, 25, 30, 35, 40, 45])
+obj = RegressionReport(tests)
+print("Average execution time:", obj.average_time())
+print("Maximum execution time:", obj.max_time())
+print("Tests slower than 25:", obj.slow_test(25))
