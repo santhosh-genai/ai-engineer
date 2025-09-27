04be99531df380985bd7ffe84335b57ccb411799
Santhosh Manickam <mailtomsanthosh@gmail.com>
2025-09-27 10:47:41 +0530

    pandas

--- python/classes/pandas/pandas_dataframe_defects.py
+++ python/classes/pandas/pandas_dataframe_defects.py
@@ -15,3 +15,19 @@ print("\nDataFrame with High Severity Defects:")
 print(df[df['Severity']=='High'])
 print("\nCount of Defects by Status:")
 print(df.groupby('Status')['DefectID'].count())
+
+df.to_csv('defects.csv', index=False)
+print("\nDataFrame saved to 'defects.csv'")
+
+new_data = {'DefectID': ['D6', 'D7'],
+            'Module': ['Login', 'Payment'],
+            'Severity': ['Low', 'High'],
+            'Status': ['Open', 'Closed']}
+df2 = pd.DataFrame(new_data)
+df2.to_csv('defects.csv', mode='a', index=True, header=False)
+print(df2)
+print("\nNew DataFrame saved to 'defects.csv'")
+
+df1 = pd.read_csv('defects.csv')
+print("\nDataFrame read from 'defects.csv':")
+print(df1)
